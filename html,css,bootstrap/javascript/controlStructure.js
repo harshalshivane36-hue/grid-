6feef45a9223f2d71d8324ce0c134544f068c46c
harshal shivane <harshalshivane36@gmail.com>
2026-09-17 19:10:54 +0530

    update code

---
+++ html,css,bootstrap/javascript/controlStructure.js
@@ -0,0 +1,13 @@
+name="Harshal"
+// var old
+// let
+// const
+let age=20
+age=20
+const Harshalfullname="Harshal Shivane"
+// conditions
+// if ,else if,else
+
+    isloggedin=true
+    num % 2 ==0
+    
---
+++ html,css,bootstrap/javascript/progOne.js
@@ -0,0 +1,12 @@
+console.log("hello from js file")
+alert("hello from js file")
+document.write("hello from document.write")
+
+
+function harshal(){
+document.write("hello from harshal function")
+    
+}
+document.getElementById("username").textContent="Harshal"
+<b>
+    </b>
